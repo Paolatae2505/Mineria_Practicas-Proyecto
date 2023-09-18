fba5183c2c309cae82d2cf9c38dff625a1ffcb14
BritnyBrito <73249749+BritnyBrito@users.noreply.github.com>
2023-09-18 12:21:59 -0600

    Create dependencias.sql

---
+++ Tarea02/dependencias.sql
@@ -0,0 +1,7 @@
+SELECT zip, city FROM dbo.hospital ORDER BY zip;
+SELECT zip, state FROM dbo.hospital ORDER BY zip;
+SELECT phone, zip FROM dbo.hospital ORDER BY phone;
+SELECT phone, city FROM dbo.hospital ORDER BY phone;
+SELECT phone, state FROM dbo.hospital ORDER BY phone;
+SELECT providernumber, measurecode, stateavg FROM dbo.hospital ORDER BY providernumber, measurecode;
+SELECT state, measurecode, stateavg FROM dbo.hospital ORDER BY  state, measurecode;
