3aabea9e215e7b252d544e1bd17345835855c02c
BritnyBrito <73249749+BritnyBrito@users.noreply.github.com>
2023-10-30 15:59:32 -0600

    inciso3.R

--- Practica07/Programa2.R
+++ Practica07/Programa2.R
@@ -8,3 +8,7 @@ grupoB <- read.csv("./grupoB.csv")
 # 2. Mostrar los datos de la variable Estatura en un histograma para cada grupo
 hist(grupoA$Estatura, main="Histograma de Estatura - Grupo A", xlab="Estatura", col="pink")
 hist(grupoB$Estatura, main="Histograma de Estatura - Grupo B", xlab="Estatura", col="thistle")
+
+# 3. box plot de variable edad para ambos grupos
+box_A_edad <- boxplot(grupoA$Edad, main="Grupo A: Edad")
+box_B__edad <- boxplot(grupoB$Edad, main="Grupo B: Edad")
