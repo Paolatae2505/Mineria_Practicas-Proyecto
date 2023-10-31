e7a47b7de63059ae0d8444d12fd45372793b385c
janet1204 <janet1204@debian.Illescas>
2023-10-31 17:13:32 -0600

    punto 4

--- Practica07/Programa/Programa2.R
+++ Practica07/Programa/Programa2.R
@@ -12,3 +12,18 @@ hist(grupoB$Estatura, main="Histograma de Estatura - Grupo B", xlab="Estatura", 
 # 3. box plot de variable edad para ambos grupos
 box_A_edad <- boxplot(grupoA$Edad, main="Grupo A: Edad")
 box_B__edad <- boxplot(grupoB$Edad, main="Grupo B: Edad")
+
+# 4. ¿Cuál grupo tiene un promedio de altura mayor? Y ¿Cuál tiene una altura mediana menor?
+meanA = mean(grupoA$Estatura)
+meanB = mean(grupoB$Estatura)
+mayor = max(meanA, meanB)
+print(paste("Promedio de altura grupo A: ", meanA))
+print(paste("Promedio de altura grupo B: ",meanB))
+print(paste("Promedio de altura mayor: ",mayor))
+
+medianA = median(grupoA$Estatura)
+medianB = median(grupoB$Estatura)
+menor = min(meanA, meanB)
+print(paste("Mediana de altura grupo A: ", medianA))
+print(paste("Mediana de altura grupo B: ",medianB))
+print(paste("Mediana de altura menor: ", menor))
