8f9774b6c9db43d780959966dcb8528b83ea3ded
victoriaebaronh <72566279+victoriaebaronh@users.noreply.github.com>
2023-10-31 17:33:58 -0600

    ejercicio 1

--- Practica07/Programa/Programa2.R
+++ Practica07/Programa/Programa2.R
@@ -5,6 +5,17 @@ library(ggplot2)
 grupoA <- read.csv("./grupoA.csv")
 grupoB <- read.csv("./grupoB.csv")
 
+# 1. Muestra la distribución de tipos sanguíneos de la variable Gr_Sang mediante una gráfica de pay.
+dist_grupoA <- prop.table(table(grupoA$Gr_sang))
+pie(dist_grupoA, col = rainbow(length(dist_grupoA)), main = "Distribución de Tipos Sanguíneos - Grupo A")
+legend("topright", legend = paste(names(dist_grupoA), ": ", round(100 * dist_grupoA, 2), "%"), 
+       fill = rainbow(length(dist_grupoA)), cex = 0.8)
+
+dist_grupoB <- prop.table(table(grupoB$Gr_sang))
+pie(dist_grupoB, col = rainbow(length(dist_grupoB)), main = "Distribución de Tipos Sanguíneos - Grupo B")
+legend("topright", legend = paste(names(dist_grupoB), ": ", round(100 * dist_grupoB, 2), "%"), 
+       fill = rainbow(length(dist_grupoB)), cex = 0.8)
+
 # 2. Mostrar los datos de la variable Estatura en un histograma para cada grupo
 hist(grupoA$Estatura, main="Histograma de Estatura - Grupo A", xlab="Estatura", col="pink")
 hist(grupoB$Estatura, main="Histograma de Estatura - Grupo B", xlab="Estatura", col="thistle")
