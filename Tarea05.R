952a8554d7e59cf8585035b1667254102ab42751
janet1204 <janet1204@debian.Illescas>
2023-11-06 22:55:33 -0600

    ejercicio d

--- Tarea05.R
+++ Tarea05.R
@@ -2,6 +2,10 @@
 install.packages("FSelector")
 install.packages("ade4")
 install.packages("GGally")
+install.packages("HSAUR")
+install.packages("car")
+install.packages("FactoMineR")
+install.packages("factoextra")
 library(FSelector)
 library(ade4)
 library(GGally)
@@ -9,6 +13,39 @@ library(GGally)
 # leer datos
 data(olympic)
 head(olympic)
+
+# EJERCICIO D
+
+# Hacemos una copia en la que solo tomamos los valores de tab para el decatlón
+decathlon <- data.frame(olympic$tab)
+head(decathlon)
+
+#Obtenemos un resumen de los datos del decatlón
+summary(decathlon)
+
+#Vamos a realizar un diagrama de dispersión
+library(car)
+
+#Recodificamos para tener todas en la misma dirección
+decathlon$X100 <- max(decathlon$X100)-decathlon$X100
+decathlon$long <- max(decathlon$long)-decathlon$long
+decathlon$poid <- max(decathlon$poid)-decathlon$poid
+decathlon$haut <- max(decathlon$haut)-decathlon$haut
+decathlon$X400 <- max(decathlon$X400)-decathlon$X400
+decathlon$X110 <- max(decathlon$X110)-decathlon$X110
+decathlon$disq <- max(decathlon$disq)-decathlon$disq
+decathlon$perc <- max(decathlon$perc)-decathlon$perc
+decathlon$jave <- max(decathlon$jave)-decathlon$jave
+decathlon$X1500 <- max(decathlon$X1500)-decathlon$X1500
+
+#Dibujamos el diagrama de dispersión correspondiente
+scatterplotMatrix(decathlon,diagonal = list(method ="histogram"),smooth = FALSE)
+
+#Comprobemos los resultados con la matriz de correlaciones
+round(cor(decathlon),2)
+
+# EJERCICIO E
+
 # hacemos una copia en la cual cambiamos el formato de olympyc para facilitar 
 # usar las funciones de FSelector
 olymic_combined <- data.frame(olympic$tab, olympic$score)
