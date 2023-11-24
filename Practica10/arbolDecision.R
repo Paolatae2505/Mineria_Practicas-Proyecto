317219db4043deaba0134027e23f1fdfef871e4a
Paola V.B <Paola_VB@ciencias.unam.mx>
2023-11-23 18:48:15 -0600

    Avances

--- Practica10/arbolDecision.R
+++ Practica10/arbolDecision.R
@@ -1,8 +1,10 @@
+# Instalar y cargar paquetes
 install.packages("rpart")
 install.packages("rpart.plot")
 library(rpart)
 library(rpart.plot)
-# Enumerar las opciones
+
+# Enumerar las opciones , en este caso son 2
 opciones <- c("Opción 1", "Opción 2")
 
 # Enlistar las alternativas de decisión y sus estados asociados
@@ -31,19 +33,22 @@ datos <- data.frame(
 modelo_arbol <- rpart(Opcion ~ Probabilidad + PorcentajeCorrecto, data = datos, method = "class")
 
 # Visualizar el árbol de decisión
-rpart.plot(modelo_arbol, main = "Árbol de Decisión para la Elección de Opción")
+rpart.plot(modelo_arbol, main = "Árbol de Decisión")
 
+# Ajustar los costos
+costo_opcion1 <- 450000  # costo original
+costo_opcion2 <- 600000  # costo original
 
-# Asignar probabilidades a priori a cada estado
+# Ajustar las probabilidades a priori
 prob_priori_estado1 <- 0.5
 prob_priori_estado2 <- 0.5
 
 # Calcular beneficio de cada rama
-beneficio_rama_opcion1_estado1 <- prob_priori_estado1 * 0.8 * 0.7 * 250 + prob_priori_estado2 * 0.2 * 0.5 * 250 - 450000
-beneficio_rama_opcion1_estado2 <- prob_priori_estado1 * 0.8 * 0.3 * 250 + prob_priori_estado2 * 0.2 * 0.5 * 250 - 450000
+beneficio_rama_opcion1_estado1 <- prob_priori_estado1 * (0.8 * 0.7 * 250 - costo_opcion1) + prob_priori_estado2 * (0.2 * 0.5 * 250 - costo_opcion1)
+beneficio_rama_opcion1_estado2 <- prob_priori_estado1 * (0.8 * 0.3 * 250 - costo_opcion1) + prob_priori_estado2 * (0.2 * 0.5 * 250 - costo_opcion1)
 
-beneficio_rama_opcion2_estado1 <- prob_priori_estado1 * 0.7 * 0.7 * 250 + prob_priori_estado2 * 0.3 * 0.5 * 250 - 600000
-beneficio_rama_opcion2_estado2 <- prob_priori_estado1 * 0.7 * 0.3 * 250 + prob_priori_estado2 * 0.3 * 0.5 * 250 - 600000
+beneficio_rama_opcion2_estado1 <- prob_priori_estado1 * (0.7 * 0.7 * 250 - costo_opcion2) + prob_priori_estado2 * (0.3 * 0.5 * 250 - costo_opcion2)
+beneficio_rama_opcion2_estado2 <- prob_priori_estado1 * (0.7 * 0.3 * 250 - costo_opcion2) + prob_priori_estado2 * (0.3 * 0.5 * 250 - costo_opcion2)
 
 # Resolver el árbol de decisión de derecha a izquierda
 beneficio_opcion1 <- max(beneficio_rama_opcion1_estado1, beneficio_rama_opcion1_estado2)
@@ -55,3 +60,4 @@ if (beneficio_opcion1 > beneficio_opcion2) {
 } else {
   cat("La mejor opción es la Opción 2 con un beneficio de", round(beneficio_opcion2, 2), "\n")
 }
+
--- ProyectoFinal/prepDatosTerrorismo.R
+++ ProyectoFinal/prepDatosTerrorismo.R
@@ -1,5 +1,6 @@
-
+# Carga de datos
 gtd_data <- read.csv("/home/paola/Documentos/SeptimoSemestre/MYAD/ProyectoFinal/globalterrorismdb_0718dist.csv")
+
 # Muestreo del 10% de tus datos
 porcentaje_muestreo <- 0.1
 tamano_muestra <- round(nrow(gtd_data) * porcentaje_muestreo)
@@ -13,11 +14,26 @@ muestra <- gtd_data[sample(nrow(gtd_data), tamano_muestra), ]
 # Muestra el resumen de la muestra
 summary(muestra)
 
-
-
-#Elimincación de valores atipicos
+# Eliminación de valores atípicos en cada columna numérica
 columnas_numericas <- sapply(muestra, is.numeric)
-muestra_numericas <- muestra[, columnas_numericas]
 
-# Eliminar filas con valores atípicos
-muestra <- muestra[!apply(muestra_numericas, 2, function(x) any(abs(scale(x)) > 3)), ]
+for (columna in colnames(muestra)[columnas_numericas]) {
+  # Calcular el primer y tercer cuartil
+  Q1 <- quantile(muestra[, columna], 0.25)
+  Q3 <- quantile(muestra[, columna], 0.75)
+  
+  # Calcular el rango intercuartíl (IQR)
+  IQR_valor <- Q3 - Q1
+  
+  # Definir el umbral basado en el IQR
+  umbral_atipico <- 1.5 * IQR_valor
+  
+  # Identificar índices de valores atípicos
+  indices_atipicos <- which(muestra[, columna] < (Q1 - umbral_atipico) | muestra[, columna] > (Q3 + umbral_atipico))
+  
+  # Filtrar el conjunto de datos para quitar los valores atípicos
+  muestra <- muestra[-indices_atipicos, ]
+}
+
+# Muestra el resumen después de la eliminación de valores atípicos
+summary(muestra)
