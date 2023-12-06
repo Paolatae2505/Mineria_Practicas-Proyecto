4d55f3bc90fafec6b1273deb52be7bc1c0038052
BritnyBrito <73249749+BritnyBrito@users.noreply.github.com>
2023-12-06 09:48:43 -0600

    kmeans bonito

--- ProyectoFinal/k-means.R
+++ ProyectoFinal/k-means.R
@@ -1,68 +1,15 @@
-# --- A CAMBIAR UNA VEZ QUE SE TENGA EL csv DE LOS PREPROCESADOS ---
-gtd_data <- read.csv("/home/paola/Documentos/SeptimoSemestre/MYAD/ProyectoFinal/globalterrorismdb_0718dist.csv")
-# NA's
-mode2 <- function(x) {
-  ux <- unique(x)
-  ux[which.max(tabulate(match(x, ux)))]
-}
-imputacion <- function(data){
-  for (var in 1:ncol(data)) {
-    if (class(data[,var])=="numeric") {
-      data[is.na(data[,var]),var] <- mean(data[,var], na.rm = TRUE)
-    } else if (class(data[,var]) %in% c("character", "factor")) {
-      no_empty <- na.omit(data[,var][data[,var] != ""])
-      m <- mode2(no_empty)
-      data[is.na(data[,var]),var] <- m
-      data[data[,var]== "",var] <- m
-    } else if (class(data[,var]) == "integer"){
-      data[is.na(data[,var]),var] <- median(data[,var], na.rm = TRUE)
-    }
-  }
-  return(data)
-}
-gtd_data <-imputacion(gtd_data)
+# importamos los datos, todos en formato numerico y normalizados
+gtd_data <- read.csv("/gtd_data_normalizado_all.csv")
 
-# Muestreo del 1% (motivos de hardware)
-porcentaje_muestreo <- 0.01
-tamano_muestra <- round(nrow(gtd_data) * porcentaje_muestreo)
-# Configuramos una semilla para reproducibilidad
-set.seed(123)
-# Realizamos el muestreo
-gtd_data_m <- gtd_data[sample(nrow(gtd_data), tamano_muestra), ]
-
-# convertir a numeric los character y factor
-gtd_data_numeric<-data.frame(gtd_data_m)
-cols_names <- colnames(gtd_data)
-j <- 1
-for(i in gtd_data_numeric) {
-  n = nlevels(i)
-  if(class(i) == 'character'){
-      x <- as.factor(i)
-      gtd_data_numeric[cols_names[j]]<- as.numeric(x)
-  }
-  if(class(i) == 'factor'){
-    gtd_data_numeric[cols_names[j]]<- as.numeric(i) #as.factor(sample(i, n, replace=TRUE))
-  }
-  j <-j+1
-}
-
-#noramlización min-max
-min.max <- function(x) {
-  return ((x - min(x)) / (max(x) - min(x)))
-}
-# aplicamos la función
-normalizados <- as.data.frame(lapply(gtd_data_numeric,min.max))
-
-normalizados <- subset(normalizados, select = -c(gsubname3, weaptype4, weaptype4_txt, weapsubtype4, weapsubtype4_txt,guncertain3,ransompaidus,claimmode3))
-head(normalizados)
-
-# --- APLICACION ALGORTIMO k-means 
-
-# Dependencias
+# importar dependencias
+install.packages("readxl")
 library(readxl)
 library(tibble)
+#install.packages("tidyverse")
 library(tidyverse)
 library(cluster)
+library(magrittr) 
+library(dplyr)    
 #install.packages("textshape")
 library(textshape)
 #install.packages("NbClust")
@@ -72,69 +19,49 @@ library(tidyr)
 library(factoextra)
 
 
-#calcular la matriz de distacias
-#el método aceptado también puede ser: "maximum", "manhattan", "canberra",
-#"binary", "minkowski", "pearson", "spearman" o "kendall"
-m.distancia <- get_dist(normalizados, method = "euclidean")
+# calcular la matriz de distacias
+m.distancia <- get_dist(gtd_data, method = "euclidean")
 fviz_dist(m.distancia, gradient = list(low = "blue", mid = "white", high = "red"))
 
 #estimar el número de clústers
-#el método aceptado puede ser: wss, elbow, silhouette o gap_stat
-fviz_nbclust(normalizados, kmeans, method = "wss")
-fviz_nbclust(normalizados, kmeans, method = "silhouette")
-fviz_nbclust(normalizados, kmeans, method = "gap_stat")
+fviz_nbclust(gtd_data, kmeans, method = "wss")
+fviz_nbclust(gtd_data, kmeans, method = "silhouette")
+fviz_nbclust(gtd_data, kmeans, method = "gap_stat")
 
-#con esta función se pueden calcular los siguientes métodos: ARREGLAR
-#"kl", "ch", "hartigan", "ccc", "scott", "marriot", "trcovw", "tracew",
-#"friedman", "rubin", "cindex","db", "silhouette", "duda", "pseudot2", "beale",
-#"ratkowsky", "ball","ptbiserial", "gap", "frey", "mcclain", "gamma", "gplus",
-#"tau", "dunn","hubert", "sdindex", "dindex", "sdbw",
-#"all" (all indices except GAP, Gamma, Gplus and Tau)
-#"alllong" (es decir, todos los antes citados).
-subset_df <- normalizados[, c("iyear", "country", "attacktype1")]
-porcentaje_muestreo <- 0.5
-tamano_muestra <- round(nrow(subset_df) * porcentaje_muestreo)
-# Realizamos el muestreo
-subset_df <- subset_df[sample(nrow(subset_df), tamano_muestra), ]
-resnumclust<-NbClust(subset_df, distance = "euclidean", min.nc=2, max.nc=5, method = "kmeans", index = "alllong")
-fviz_nbclust(resnumclust)
-
-#con el resultado anterior observamos que el mejor candidato es 3
-#por lo tanto, calculamos a dos clústers
-k3 <- kmeans(normalizados, centers = 3, nstart = 25)
+# Con el resultado anterior observamos que el mejor candidato es 3
+# Calculamos 3 clústers
+k3 <- kmeans(gtd_data, centers = 3, nstart = 25)
 str(k3)
 
-#ploteamos los cluster
-fviz_cluster(k3, data = normalizados)
-library(magrittr) # needs to be run every time you start R and want to use %>%
-library(dplyr)    # alternatively, this also loads %>%
+# Ploteamos los cluster
+fviz_cluster(k3, data = gtd_data)
 
-#regresamos al df original para poder continuar el analisis
-normalizados %>%
+# Regresamos al data frames original para continuar el analisis
+gtd_data %>%
   mutate(Cluster = k3$cluster) %>%
   group_by(Cluster) %>%
   summarise_all("mean")
+gtd_data_clusters <- gtd_data
+head(gtd_data_clusters)
 
-df <- normalizados
-head(df)
-#aplicamos la función factor al cluster obtenidos
-df$clus<-as.factor(k3$cluster)
+# Aplicamos la función factor a los clusters obtenidos
+gtd_data_clusters$clus <- as.factor(k3$cluster)
+head(gtd_data_clusters)
 
-#volvemos a iniciar el df con los datos originales
-df <- normalizados
-#estandarizamos el df
-df <- scale(df)
-df<- as.data.frame(df)
-#volvemos a aplicar factor
-df$clus<-as.factor(k3$cluster)
-#y mostramos el resultado
-#head(df)
-library(tidyr)
-df$clus<-factor(df$clus)
-#del df lo modificamos las columnas para agregar el cluster al que pertenece
-data_long <- gather(df, caracteristica, valor, success:1.0000, factor_key=TRUE)
+# Volvemos a iniciar el gtd_data_clusters con los datos originales
+gtd_data_clusters <- gtd_data
+# Estandarizamos el gtd_data_clusters
+gtd_data_clusters <- scale(gtd_data_clusters)
+gtd_data_clusters <- as.data.frame(gtd_data_clusters)
+#Volvemos a aplicar factor
+gtd_data_clusters$clus<-as.factor(k3$cluster)
+head(gtd_data_clusters)
+
+gtd_data_clusters$clus <- factor(gtd_data_clusters$clus)
+# gtd_data_clusters: modificamos las columnas para agregar el cluster al que pertenece cada tupla
+data_long <- gather(gtd_data_clusters, caracteristica, valor, success:1.0000, factor_key=TRUE)
 head(data_long)
-#por último, mostramos los resultados de la modificación anterior ARREGLAR
+# Mostramos los resultados de la modificación anterior 
 ggplot(data_long, aes(x = as.factor(caracteristica),
                       y = valor,group=clus, colour = clus))+
   stat_summary(fun = mean, geom="pointrange", size = 1)+
